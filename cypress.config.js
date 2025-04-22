efc25f26a30e4f7cecfebc831867ebf19c04f703
QA <qa@QAui-MacBookPro.local>
2025-04-22 12:07:01 +0900

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportWidth: process.env.GITHUB_ACTIONS ? 1920 : 1920,
+  viewportHeight: process.env.GITHUB_ACTIONS ? 1080 : 1080,
+  pageLoadTimeout: 60 * 1000,
+  redirectionLimit: 30,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      
     },
   },
 });
