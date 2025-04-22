33afb4bc6f59c5859c75d37afa934a5cb83c65ea
QA <73218016+Bread-kn72@users.noreply.github.com>
2025-04-22 15:03:40 +0900

    install dotenv

--- cypress/e2e/GNB,LNB.cy.js
+++ cypress/e2e/GNB,LNB.cy.js
@@ -1,13 +1,16 @@
-const { loginModule } = require('./module/manager.module.js');
-
 describe('GNB,LNB Test', () => {
 
+    beforeEach(() => {
+        const site = Cypress.env('stgNewCeo');
+
+        cy.visit(site);
+        cy.log(site)
+        cy.wait(1 * 1000)
+        cy.get(':nth-child(1) > .css-7boy05 > .css-1ajjw8p').type(`${Cypress.env('stgID')}`)
+        cy.get(':nth-child(1) > .css-7boy05 > .css-1ajjw8p').type(`${Cypress.env('stgPW')}`)
+      });
+
     it('GNB', () => {
-        loginModule.login({
-            Site: `${Cypress.env('stgNewCeo')}`,
-            Id: `${Cypress.env('stgID')}`,
-            Password: `${Cypress.env('stgPW')}`,
-        });
 
         cy.contains('전체 브랜드').click();
         cy.wait(1 * 1000);
@@ -17,11 +20,6 @@ describe('GNB,LNB Test', () => {
     });
 
     it('LNB', () => {
-        loginModule.login({
-            Site: `${Cypress.env('stgNewCeo')}`,
-            Id: `${Cypress.env('stgID')}`,
-            Password: `${Cypress.env('stgPW')}`,
-        });
 
         /* 매출분석 */
         cy.contains('매출분석').click();
