a00fdd3f525ed625ead649f2f32a7596139289ec
QA <73218016+Bread-kn72@users.noreply.github.com>
2025-04-23 09:28:05 +0900

    Update login.module.js

--- cypress/e2e/module/login.module.js
+++ cypress/e2e/module/login.module.js
@@ -1,18 +1,12 @@
 function login({ Site, Id, Password }) {
-    if (Site === Cypress.env("stgNewCeo")) {
-      cy.visit(Site);
-  
-      cy.get(":nth-child(1) > .css-7boy05 > .css-1ajjw8p", { timeout: 30 * 1000 })
-        .clear()
-        .type(Id); // ID 입력
-  
-      cy.get(":nth-child(2) > .css-7boy05 > .css-1ajjw8p", { timeout: 30 * 1000 })
-        .clear()
-        .type(Password); // 비밀번호 입력
-  
+  if (Site === Cypress.env('stgNewCeo')) {
+    return cy.visit(Site).then(() => {
+      cy.get(":nth-child(1) > .css-7boy05 > .css-1ajjw8p", { timeout: 30 * 1000 }).clear().type(Id);
+      cy.get(":nth-child(2) > .css-7boy05 > .css-1ajjw8p", { timeout: 30 * 1000 }).clear().type(Password);
       cy.get(".css-2ptubj", { timeout: 30 * 1000 }).click();
-    }
+    });
   }
+}
   
   module.exports = {
     login,
