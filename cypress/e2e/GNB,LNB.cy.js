831061479d555dfc4b0bfa7692967f9a5a8b4114
QA <73218016+Bread-kn72@users.noreply.github.com>
2025-04-23 09:28:22 +0900

    update testcase

--- cypress/e2e/GNB,LNB.cy.js
+++ cypress/e2e/GNB,LNB.cy.js
@@ -1,57 +1,52 @@
-describe('GNB,LNB Test', () => {
+const { loginModule } = require('./module/manager.module.js');
 
-    beforeEach(() => {
-        const site = Cypress.env('stgNewCeo');
-
-        cy.visit(site);
-        cy.log(site)
-        cy.wait(1 * 1000)
-        cy.get(':nth-child(1) > .css-7boy05 > .css-1ajjw8p').type(`${Cypress.env('stgID')}`)
-        cy.get(':nth-child(2) > .css-7boy05 > .css-1ajjw8p').type(`${Cypress.env('stgPW')}`)
-        cy.get('.css-2ptubj').click();
-
-      });
-
-    it('GNB', () => {
-
-        cy.contains('전체 브랜드').click();
-        cy.wait(1 * 1000);
-        cy.contains('관리 전환').click();
-        cy.contains('내정보');
-        cy.contains('로그아웃');
+describe("GNB,LNB Test", () => {
+  beforeEach(() => {
+    loginModule.login({
+      Site: Cypress.env('stgNewCeo'),
+      Id: Cypress.env('stgID'),
+      Password: Cypress.env('stgPW'),
     });
+  });
 
-    it('LNB', () => {
+  it("GNB", () => {
+    cy.contains("전체 브랜드").click();
+    cy.wait(1 * 1000);
+    cy.contains("관리 전환").click();
+    cy.contains("내정보");
+    cy.contains("로그아웃");
+  });
 
-        /* 매출분석 */
-        cy.contains('매출분석').click();
-        cy.wait(1 * 1000);
-        cy.contains('매장별 매출').click();
-        cy.contains('기간별 매출').click();
-        cy.contains('플랫폼별 매출').click();
-        cy.contains('카테고리별 매출').click();
-        cy.contains('상품별 매출').click();
-        /* 매장관리 */
-        cy.contains('매장관리').click();
-        cy.wait(1 * 1000);
-        cy.contains('연동정보 관리').click();
-        cy.contains('기본정보 관리').click();
-        cy.contains('영업시간 관리').click();
-        cy.contains('주문정보 관리').click();
-        /* 메뉴 관리 */
-        cy.contains('메뉴관리').click();
-        cy.wait(1 * 1000);
-        cy.contains('상품관리').click();
-        cy.contains('카테고리 관리').click();
-        cy.contains('메뉴판 관리').click();
-        cy.contains('옵션 관리').click();
-        /* 주문관리 */
-        cy.contains('주문관리').click();
-        cy.wait(1 * 1000);
-        cy.contains('주문내역').click();
-        /* 권한관리 */
-        cy.contains('권한관리').click();
-        cy.wait(1 * 1000);
-        cy.contains('요청관리').click();
-    });
+  it("LNB", () => {
+    /* 매출분석 */
+    cy.contains("매출분석").click();
+    cy.wait(1 * 1000);
+    cy.contains("매장별 매출").click();
+    cy.contains("기간별 매출").click();
+    cy.contains("플랫폼별 매출").click();
+    cy.contains("카테고리별 매출").click();
+    cy.contains("상품별 매출").click();
+    /* 매장관리 */
+    cy.contains("매장관리").click();
+    cy.wait(1 * 1000);
+    cy.contains("연동정보 관리").click();
+    cy.contains("기본정보 관리").click();
+    cy.contains("영업시간 관리").click();
+    cy.contains("주문정보 관리").click();
+    /* 메뉴 관리 */
+    cy.contains("메뉴관리").click();
+    cy.wait(1 * 1000);
+    cy.contains("상품관리").click();
+    cy.contains("카테고리 관리").click();
+    cy.contains("메뉴판 관리").click();
+    cy.contains("옵션 관리").click();
+    /* 주문관리 */
+    cy.contains("주문관리").click();
+    cy.wait(1 * 1000);
+    cy.contains("주문내역").click();
+    /* 권한관리 */
+    cy.contains("권한관리").click();
+    cy.wait(1 * 1000);
+    cy.contains("요청관리").click();
+  });
 });
--- cypress/e2e/Mypage.cy.js
+++ cypress/e2e/Mypage.cy.js
@@ -1,50 +1,53 @@
-const { loginModule } = require('../module/manager.module.js');
+const { loginModule } = require('./module/manager.module.js');
 
-describe('SignUp', () => {
-    /* 마스터 */
-    it('Mypage', () => {
+describe("Mypage", () => {
+    beforeEach(() => {
         loginModule.login({
-            Site: `${Cypress.env('stgNewCeo')}`,
-            Id: `${Cypress.env('monkistore')[0]}`,
-            Password: `${Cypress.env('TestPwd')}`,
+          Site: Cypress.env("stgNewCeo"),
+          Id: Cypress.env("stgID1"),
+          Password: Cypress.env("stgPW1"),
         });
+      });
 
-        // 내정보 선택
-        cy.contains('내정보').click();
-        cy.wait(1 * 1000);
+  /* 마스터 */
+  it("Mypage", () => {
 
-        //비밀번호 입력
-        cy.get('.css-1bq4gr1').type(`${Cypress.env('TestPwd')}`);
-        cy.get('.css-1fndwrv').contains('확인').click();
+    // 내정보 선택
+    cy.contains("내정보").click();
+    cy.wait(1 * 1000);
 
-        //정보 수정 진입
-        cy.get('.css-18uwtlr').contains('정보 수정').click();
+    //비밀번호 입력
+    cy.get(".css-1bq4gr1").type(Cypress.env("stgPW1"));
+    cy.get('.css-1fndwrv').contains("확인").click();
 
-        //PW 수정
-        cy.get('.css-17tvlxz').contains('비밀번호 변경').click();
-        cy.get('#password').type(`${Cypress.env('TestPwd')}`);
-        cy.get('#passwordConfirm').type(`${Cypress.env('TestPwd')}`);
-        cy.get('.css-2ptubj').contains('button', '확인').click();
+    //정보 수정 진입
+    cy.get(".css-18uwtlr").contains("정보 수정").click();
 
-        //Email 변경
-        cy.get('#userEmail')
-            .clear()
-            .type(`${Cypress.env('TestEmail')}`);
+    //PW 수정
+    cy.get(".css-17tvlxz").contains("비밀번호 변경").click();
+    cy.get("#password").clear().type(Cypress.env("stgPW1"));
+    cy.get("#passwordConfirm").clear().type(Cypress.env("stgPW1"));
+    cy.get(".css-2ptubj").contains("button", "확인").click();
 
-        //대표자 변경
-        cy.get('#userName').clear().type('황예진');
+    //Email 변경
+    cy.get("#userEmail")
+      .clear()
+      .type(Cypress.env("stgEmail"));
 
-        //사업자등록증 변경
-        cy.fixture('image/로고이미지/0000.png', 'base64').then(fileContent => {
-            cy.get('input[type="file"][id="bizCertificateFile"]').attachFile({
-                fileContent,
-                filePath: 'image/로고이미지/0000.png',
-                fileName: '0000.png',
-                mimeType: 'image/png',
-            });
-        });
+    //대표자 변경
+    cy.get("#userName").clear().type("방기남");
 
-        //정보 저장
-        cy.contains('button', '정보 저장').click();
+    //사업자등록증 변경
+    cy.fixture("image/로고이미지/0000.png", "base64").then((fileContent) => {
+      cy.get('input[type="file"][id="bizCertificateFile"]').attachFile({
+        fileContent,
+        filePath: "image/로고이미지/0000.png",
+        fileName: "0000.png",
+        mimeType: "image/png",
+      });
     });
+
+    //정보 저장
+    cy.contains("button", "정보 저장").click();
+  });
 });
